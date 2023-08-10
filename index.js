1cbf5659d8032a69d7bba6afee13556acc76a8d4
Sairam <100087362+sairam1800@users.noreply.github.com>
2023-08-10 20:28:24 +0530

    cors

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ mongoose
   .catch((err) => console.log(err));
 
 // res.header("Access-Control-Allow-Origin", "true");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
